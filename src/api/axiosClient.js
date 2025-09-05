28d2a6440bfeafe0f8154dc1f52cde69983af2c0
phamhoanghuy2004 <phamhoanghuy.2000vn@gmail.com>
2025-09-05 15:00:29 +0700

    huy update axio

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -4,7 +4,8 @@ const axiosClient = axios.create({
     baseURL: "http://localhost:3000/api",
     headers: {
     "Content-Type": "application/json",
-  }
+  },
+  withCredentials: true
 })
 axiosClient.interceptors.request.use(
   (config) => {
