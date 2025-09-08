fa893ae573b19c22b3de10622645332be576274c
hiendangba <22110320@student.hcmute.edu.vn>
2025-09-08 08:41:18 +0700

    update PORT 3001

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "http://localhost:3001/api",
     headers: {
     "Content-Type": "application/json",
-  }
+  },
+  withCredentials: true
 })
 axiosClient.interceptors.request.use(
   (config) => {
